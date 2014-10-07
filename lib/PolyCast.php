51dee3bacb1f81666c73d632011be24181660b35
Theodore Brown <Theodore.Brown@netgainhosting.com>
2014-10-07 09:13:41 -0500

    to_int should ensure the value isn't less than PHP_INT_MIN
    Also fixed non-64-bit PHP_INT_MAX check

--- lib/PolyCast.php
+++ lib/PolyCast.php
@@ -11,11 +11,21 @@ function to_int($val)
         case "integer":
             return $val;
         case "double":
-            if (!is_infinite($val) && !is_nan($val) && $val < PHP_INT_MAX) {
-                return (int) $val; // valid floats should cast
-            } else {
-                return false;
+            $PHP_INT_MIN = ~PHP_INT_MAX; // const isn't in PHP 5.x
+
+            if (!is_infinite($val) && !is_nan($val) && $val >= $PHP_INT_MIN) {
+                // due to rounding issues, on 64-bit platforms
+                // the float must be less than PHP_INT_MAX
+
+                if (
+                    (PHP_INT_SIZE === 8 && $val < PHP_INT_MAX) || // valid 64-bit
+                    (PHP_INT_SIZE !== 8 && $val <= PHP_INT_MAX) // valid non-64-bit
+                ) {
+                    return (int) $val; // valid floats should cast
+                }
             }
+
+            return false;
         case "string":
             $val = trim($val, " \t\n\r\v\f"); // trim whitespace
             return filter_var($val, FILTER_VALIDATE_INT);
